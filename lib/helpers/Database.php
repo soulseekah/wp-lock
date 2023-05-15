c9138bd4a94856b20f6066d2797b41a6db643b9e
Igor Tron <public@itron.pro>
2023-05-15 16:23:55 +0200

    CS for helpers.

--- lib/helpers/Database.php
+++ lib/helpers/Database.php
@@ -2,57 +2,55 @@
 
 namespace soulseekah\WP_Lock\helpers;
 
-final class Database
-{
-    public static function register_table($key, $name = false)
-    {
-        global $wpdb;
+final class Database {
+	public static function register_table( $key, $name = false ) {
+		global $wpdb;
 
-        if (! $name) {
-            $name = $key;
-        }
+		if ( ! $name ) {
+			$name = $key;
+		}
 
-        $wpdb->tables[] = $name;
-        $wpdb->$key = $wpdb->prefix . $name;
-    }
+		$wpdb->tables[] = $name;
+		$wpdb->$key     = $wpdb->prefix . $name;
+	}
 
-    public static function install_table($key, $columns, $opts = [])
-    {
-        global $wpdb;
+	public static function install_table( $key, $columns, $opts = [] ) {
+		global $wpdb;
 
-        $full_table_name = $wpdb->prefix . $key;
+		$full_table_name = $wpdb->prefix . $key;
 
-        if (is_string($opts)) {
-            $opts = [ 'upgrade_method' => $opts ];
-        }
+		if ( is_string( $opts ) ) {
+			$opts = [ 'upgrade_method' => $opts ];
+		}
 
-        $opts = wp_parse_args($opts, [
-            'upgrade_method' => 'dbDelta',
-            'table_options' => '',
-        ]);
+		$opts = wp_parse_args( $opts, [
+			'upgrade_method' => 'dbDelta',
+			'table_options'  => '',
+		] );
 
-        $charset_collate = '';
-        if ($wpdb->has_cap('collation')) {
-            if (! empty($wpdb->charset)) {
-                $charset_collate = "DEFAULT CHARACTER SET $wpdb->charset";
-            }
-            if (! empty($wpdb->collate)) {
-                $charset_collate .= " COLLATE $wpdb->collate";
-            }
-        }
+		$charset_collate = '';
+		if ( $wpdb->has_cap( 'collation' ) ) {
+			if ( ! empty( $wpdb->charset ) ) {
+				$charset_collate = "DEFAULT CHARACTER SET $wpdb->charset";
+			}
+			if ( ! empty( $wpdb->collate ) ) {
+				$charset_collate .= " COLLATE $wpdb->collate";
+			}
+		}
 
-        $table_options = $charset_collate . ' ' . $opts['table_options'];
+		$table_options = $charset_collate . ' ' . $opts['table_options'];
 
-        if ('dbDelta' == $opts['upgrade_method']) {
-            require_once ABSPATH . 'wp-admin/includes/upgrade.php';
-            dbDelta("CREATE TABLE $full_table_name ( $columns ) $table_options");
-            return;
-        }
+		if ( 'dbDelta' == $opts['upgrade_method'] ) {
+			require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+			dbDelta( "CREATE TABLE $full_table_name ( $columns ) $table_options" );
 
-        if ('delete_first' == $opts['upgrade_method']) {
-            $wpdb->query("DROP TABLE IF EXISTS $full_table_name;");
-        }
+			return;
+		}
 
-        $wpdb->query("CREATE TABLE IF NOT EXISTS $full_table_name ( $columns ) $table_options;");
-    }
+		if ( 'delete_first' == $opts['upgrade_method'] ) {
+			$wpdb->query( "DROP TABLE IF EXISTS $full_table_name;" );
+		}
+
+		$wpdb->query( "CREATE TABLE IF NOT EXISTS $full_table_name ( $columns ) $table_options;" );
+	}
 }
